720b76e417a8eefc8153c752ed4eeb360fe045ab
Tamara Salinas <gatamara29@gmail.com>
2023-06-28 23:42:52 -0400

    primer commit

---
+++ src/uncontrolled-component-copy.jsx
@@ -0,0 +1,28 @@
+import './App.css'
+
+export function Uncontrolled() {
+    const submit = (e) => {
+        e.preventDefault()
+        const data = Array.from(new FormData(e.target))
+    }
+
+    return (
+        <>
+            <form onSubmit={submit}>
+                <div>
+                    <span>
+                        lala
+                    </span>
+                    <input type="text" name='campo' />
+                </div>
+                <input type="text" name='campo-2' />
+                <input type="file" />
+                <input type="submit" value='Enviar' />
+            </form>
+        </>
+    )
+}
+
+
+
+// console.log(Array.from(new FormData(e.target)));
